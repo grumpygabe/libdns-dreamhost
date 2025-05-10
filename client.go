86d792ae8cbc2900f8f15d02edc17c096d02f18c
grumpygabe <99508073+grumpygabe@users.noreply.github.com>
2025-05-10 08:37:18 -0500

    Update client.go

--- client.go
+++ client.go
@@ -16,21 +16,54 @@ func (p *Provider) init() error {
 	return nil
 }
 
+// Custom Record implementation for the Dreamhost provider
+type DreamhostRecord struct {
+	TypeValue  string
+	ValueValue string
+	NameValue  string
+	rrCache    libdns.RR
+}
+
+// RR implements the libdns.Record interface
+func (r DreamhostRecord) RR() libdns.RR {
+	return r.rrCache
+}
+
 func recordFromApiDnsRecord(apiDnsRecord api.DNSRecord) libdns.Record {
-	var rec libdns.Record
-	rec.Type = string(apiDnsRecord.Type)
-	rec.Value = apiDnsRecord.Value
-	// We need to get the name minus the zone to match what libdns expects
-	rec.Name = libdns.RelativeName(apiDnsRecord.Record, apiDnsRecord.Zone)
-	return rec
+	// Create our custom record implementation
+	record := &DreamhostRecord{
+		TypeValue:  string(apiDnsRecord.Type),
+		ValueValue: apiDnsRecord.Value,
+		NameValue:  libdns.RelativeName(apiDnsRecord.Record, apiDnsRecord.Zone),
+	}
+
+	// Initialize the RR cache
+	record.rrCache = libdns.RR{
+		Name: record.NameValue,
+	}
+
+	return record
 }
 
 func apiDnsRecordInputFromRecord(record libdns.Record, zone string) api.DNSRecordInput {
 	var recordInput api.DNSRecordInput
-	recordInput.Type = api.RecordType(record.Type)
-	recordInput.Value = record.Value
-	// Dreamhost expects the record name to be absolute, without a dot at the end
-	zone = strings.TrimRight(zone, ".")
-	recordInput.Record = libdns.AbsoluteName(record.Name, zone)
+
+	// Try to cast to our custom record type
+	dhRecord, ok := record.(*DreamhostRecord)
+	if ok {
+		recordInput.Type = api.RecordType(dhRecord.TypeValue)
+		recordInput.Value = dhRecord.ValueValue
+		// Dreamhost expects the record name to be absolute, without a dot at the end
+		zone = strings.TrimRight(zone, ".")
+		recordInput.Record = libdns.AbsoluteName(dhRecord.NameValue, zone)
+	} else {
+		// Fallback for non-dreamhost records (probably won't work well)
+		rr := record.RR()
+		recordInput.Type = api.RecordType("TXT") // Default to TXT
+		recordInput.Value = ""                   // Can't extract value
+		zone = strings.TrimRight(zone, ".")
+		recordInput.Record = libdns.AbsoluteName(rr.Name, zone)
+	}
+
 	return recordInput
 }
